644fd9408f222f2436c37e3ebbbdac07d68af0c0
saanchijainnn <saanchi2315@gmail.com>
2026-07-09 23:47:38 +0530

    part 15 started

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 const express = require('express');
 const app = express();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const path = require('path');
 const cookieParser = require('cookie-parser');
+const userModel = require('./models/user');
 
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 
-
-app.get("/", function (req, res) {
-  let token =jwt.sign({email:"saanchi2315@gmail.com"}, "secret");
-  res.cookie("token",token);
-  
-  res.send("Hello World");
-  })
-
-app.get("/read", function (req, res) {
-    let data = jwt.verify (req.cookies.token, "secret");
-    console.log(data)
-})
-
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 app.listen(3000);
