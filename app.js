de2401af49cab8abcd8faec4f50f9ed2ce8c64a6
saanchijainnn <saanchi2315@gmail.com>
2026-07-11 22:10:44 +0530

    part 15 done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const userModel = require('./models/user');
 const bcrypt= require('bcrypt')
+const jwt=require('jsonwebtoken')
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -23,12 +24,37 @@ app.post('/create',  (req, res) => {
         let createdUser= await userModel.create({
      username,email,password:hash,age
    })
+   let token =jwt.sign({email},"chuppp")
+   res.cookie("token",token)
    res.send(createdUser);
     })
    })
 
 
    
+});
+
+app.get("/login",function(req,res){
+    res.render("login")
+})
+
+app.post("/login", async function(req,res){
+    let user = await userModel.findOne({email: req.body.email}).select("password")
+    if(!user)return res.send("something went wrong ");
+   bcrypt.compare(req.body.password,user.password, function(err,result){
+    if(result) {
+        let token =jwt.sign({email:user.email},"chuppp")
+   res.cookie("token",token)
+        res.send("yes, you can login!")}
+    else res.send("something went wrong")
+   
+   })
+})
+ 
+app.get("/logout",function(req,res){
+res.cookie("token","")
+res.redirect("/")
+
 });
 
 app.listen(3000);
